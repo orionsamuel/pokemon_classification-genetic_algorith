40451d86cdded32426faebbc24ea6d03ba63948c
guiegle <guiegle@hotmail.com>
2020-04-02 22:36:29 -0300

    to-do fitness

--- src/genetic-algorithm.py
+++ src/genetic-algorithm.py
@@ -1,8 +1,9 @@
 import ga
 import pandas
+import random
 import sys
 
-db = pandas.read_csv('database/base-pokemon.csv')
+db = pandas.read_csv("database/base-pokemon.csv")
 
 def execInput():
 #read the executation input if it exists (aux)
@@ -17,9 +18,51 @@ def lstr_to_lint(slist):
         ilist.append(int(element))
     return ilist
 
+def base_gen_generator(length):
+#set a new base generation with a "lentgh" of this first population (first gen)
+    base_gen = []
+    pokemons = list(db["pokedex_number"])
+    for i in range(length):
+        new_team = [random.choice(pokemons), random.choice(pokemons), random.choice(pokemons)]
+        base_gen.append(new_team)
+    return base_gen
+
+def team_crossover(teamA, teamB):
+#making two new teams to compose the new generation
+    for k in range(0):
+        tmp = teamA[k]
+        teamA[k] = teamB[k]
+        teamB[k] = tmp
+    return [teamA, teamB]
+
+def new_gen_generator(base_gen):
+#set a new generation based on previous generation "base_gen" (crossover)
+    new_gen = base_gen
+    cross_tax = 80
+    crossovers_number = (len(base_gen)*cross_tax)/200
+    for k in range(int(crossovers_number)):
+        random_team_A = random.choice(base_gen)
+        base_gen.remove(random_team_A)
+        random_team_B = random.choice(base_gen)
+        base_gen.remove(random_team_B)
+        new_team = team_crossover(random_team_A, random_team_B)
+        new_gen.append(new_team[0])
+        new_gen.append(new_team[1])
+    return new_gen
+
+
+def fitness(gen):
+#em produção
+    return
+
 def search_counters(team):
-    print(db.head())
-    
+#application of ga
+    base_gen = base_gen_generator(5)
+    new_gen = new_gen_generator(base_gen)
+    pokemons = list(db["pokedex_number"])
+    while fitness(new_gen):
+        base_gen = new_gen
+        new_gen = new_gen_generator(base_gen)
 
 if __name__ == '__main__':
 #main
