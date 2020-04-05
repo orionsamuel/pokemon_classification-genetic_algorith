805a1841bb4334c323f9dec43a17f0f4d996f593
Samuel Oliveira <samuel@imd.ufrn.br>
2020-04-05 11:56:05 -0300

    Funções adicionadas

--- src/aco.py
+++ src/aco.py
@@ -4,31 +4,37 @@ from random import randint
 
 db = read_csv("../database/base-pokemon.csv")
 
-team_target_name = ["Bulbasaur", "Charmander", "Squirtle"]
-
-# Lista de pokemon de entrada
-team_target = []
-
-# Pega os ID dos pokemon
-cont = 0
-while (cont < 3):
-    for i in range(db.name.size):
-        if(db.name[i] == team_target_name[cont]):
-            team_target.append(db.pokedex_number[i])
-    cont += 1
-
 # Lista dos pokemon counter
 team_selection = []
 
+# Lista de pokemon de entrada
+team_target_name = ["Bulbasaur", "Charmander", "Squirtle"]
+team_target = []
+
 # Criação aleatória do primeiro time counter
-for i in range(3):
-    team_counter = randint(db.pokedex_number[0],db.pokedex_number.size)
-    team_selection.append(team_counter)
+def creat_team():
+    for i in range(3):
+        team_counter = randint(db.pokedex_number[0],db.pokedex_number.size)
+        team_selection.append(team_counter)
 
-print(team_selection)
-print(team_target_name)
-print(team_target)
+# Pega os ID dos pokemon
+def getId():
+    cont = 0
+    while (cont < 3):
+        for i in range(db.name.size):
+            if(db.name[i] == team_target_name[cont]):
+                team_target.append(db.pokedex_number[i])
+        cont += 1
 
-#if __name__ == '__main__':
-#main
+def fitness():
+    
 
+
+# main
+if __name__ == '__main__':
+    creat_team()
+    getId()
+
+    print(team_selection)
+    print(team_target_name)
+    print(team_target)
