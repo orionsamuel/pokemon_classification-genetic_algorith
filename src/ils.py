50b5db4e6cac8722ca552fab771d2f477d4bd8ac
Samuel Oliveira <samuel@imd.ufrn.br>
2020-04-05 21:41:45 -0300

    Finalizado algortimo ILS

--- src/ils.py
+++ src/ils.py
@@ -1,6 +1,6 @@
 from pandas import read_csv
-import numpy as np
 from random import randint
+import sys
 
 db = read_csv("../database/base-pokemon.csv")
 
@@ -10,16 +10,24 @@ best_team_selection = []
 best_team_selection_name = []
 
 # Inicialização da avaliação
-avaliation_global = 0
+#avaliation_global = 0
+avaliation_full = []
+avaliation = []
 
 # Lista de pokemon de entrada
-team_target_name = ["Bulbasaur", "Charmander", "Squirtle"]
+team_target_name = []
 team_target = []
+
+# Recebe como entrada os pokemon
+def execInput():
+    print("Let's input  Pokomeon GO team or a boss raid to counter!")
+    team = input("Input your team by pokedexID spliting by " "(space): ")
+    return team.split(" ")
 
 # Criação aleatória do primeiro time counter
 def creat_team():
     for i in range(3):
-        team_counter = randint(db.pokedex_number[0],db.pokedex_number.size)
+        team_counter = randint(db.pokedex_number[0],db.pokedex_number.size-1)
         team_selection.append(team_counter)
 
 # Retorna o nome do time counter
@@ -70,39 +78,46 @@ def fitness(team_selection, team_target):
     return fitness
 
 # Retorna o melhor time
-def return_best_team(avaliation):
-    if(fitness(team_selection, team_target) > avaliation):
-        avaliation = fitness(team_selection, team_target)
-        print(avaliation)
-        for i in range(len(team_selection)):
-            best_team_selection[i] = team_selection[i]
-    else:
-        local_search(team_selection)
-    return avaliation
+def return_best_team():
+    for i in range(len(avaliation_full)):
+        avaliation.append(avaliation_full[i][0])
+    best_avaliation = max(avaliation)
+    for i in range(len(avaliation_full)):
+        if(avaliation_full[i][0] == best_avaliation):
+            for j in range(3):
+                best_team_selection[j] = avaliation_full[i][1][j]
+    print("Melhor avaliação: " + str(best_avaliation))
 
 # Busca local
 def local_search(team_selection):
-    print()    
+    for  i in range(220):
+        change_num = randint(1,3)
+        for i in range(change_num):
+            counter = randint(db.pokedex_number[0],db.pokedex_number.size-1)
+            team_selection[i] = counter
+        avaliation_full.append(((fitness(team_selection, team_target)),team_selection))
+        
 
 # Função que executa tudo
-def run(avaliation):
+def run():
     creat_team()
     getId()
     for i in team_selection:
         best_team_selection.append(i)
-    alaviation = return_best_team(avaliation)
-    print(avaliation)
-    return avaliation
+    print("Time Entrada:")
+    print(team_target_name)
+    local_search(team_selection)
+    return_best_team()
+    name_team_counter()
+    print("Time Counter:")
+    print(best_team_selection_name)
 
 # main
 if __name__ == '__main__':
-    
-    avaliation_global = run(avaliation_global)
-    name_team_counter()
 
-    print("Time Entrada:")
-    print(team_target_name)
-    print("Time Counter:")
-    print(best_team_selection_name)
-    print("Fitness")
-    print(avaliation_global)
+    
+    team_target_name = execInput()
+    
+    run()
+
+    
