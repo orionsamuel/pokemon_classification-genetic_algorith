c965e4bb04ed347864c76149206fad9caaef6578
ArthurGorgonio <gorgonioarthur@gmail.com>
2020-04-11 17:09:46 -0300

    Template do código que irá capturar o tempo das execuções

---
+++ src/get_time.py
@@ -0,0 +1,31 @@
+from ils import run
+from genetic_algorithm import search_counters
+from utils import get_db, lstr_to_lint
+from pandas import write_csv
+
+counters = get_db("teams")
+
+start = []
+stop = []
+
+genetic_results = []
+search_results = []
+# lista = []
+def get_fitness(tuple):
+    return tuple[2]
+# lista.sort(key=get_fitness, reverse=True)
+
+for index, steam in counters.iterrows():
+    team_target = lstr_to_lint(steam)
+    for i in range(5):
+        start.append(time.time())
+        genetic_results.append(search_counters(team_target))
+        stop.append(time.time())
+        ## Salvando em lista
+        genetic_results.sort(key=get_fitness, reverse=True)
+        write_csv("team_fit_genetic.csv", genetic_results) ## Fazendo um append
+        start.append(time.time())
+        search_results.append(run(team_target))
+        stop.append(time.time())
+        ## Salvando em lista
+        write_csv("team_fit_local_search.csv", search_results) ## Fazendo um append
